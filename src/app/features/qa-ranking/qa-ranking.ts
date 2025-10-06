0c4b6d50a45215b7b72be132b79e3046a2260981
QuirozDev8 <10alejo32@gmail.com>
2025-10-06 16:15:09 -0500

    se implemento navbar y header

---
+++ src/app/features/qa-ranking/qa-ranking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qa-ranking',
+  imports: [],
+  templateUrl: './qa-ranking.html',
+  styleUrl: './qa-ranking.css'
+})
+export class QaRanking {
+
+}
---
+++ src/app/features/sopa-letras/sopa-letras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sopa-letras',
+  imports: [],
+  templateUrl: './sopa-letras.html',
+  styleUrl: './sopa-letras.css'
+})
+export class SopaLetras {
+
+}
